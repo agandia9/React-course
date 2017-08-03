721f4130652581e28d161d198d34497ca870cf71
agandia9 <alejandro.gandia9@gmail.com>
2017-08-03 11:50:32 +0200

    added profile and input

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,10 +1,13 @@
 import React, {Component} from 'react'
 import MessageList from '../MessageList'
+import InputText from '../InputText'
+import ProfileBar from '../ProfileBar'
 
-class Main extends Component{
+class Main extends Component {
 	constructor(){
 		super()
 		this.state = {
+			openText:false,
 			messages: [{
 				text: 'message default',
 				picture: 'https://pbs.twimg.com/profile_images/799369354878062593/9Fxts0_p_400x400.jpg',
@@ -21,9 +24,28 @@ class Main extends Component{
 			}]
 		}
 	}
+
+	handleOpenText(event){
+		event.preventDefault()
+		this.setState({openText:true})
+	}
+
+	renderOpenText(){
+		if(this.state.openText){
+			return <InputText />
+		}
+	}
 	render(){
 		return(
-			<MessageList messages={this.state.messages} />
+			<div>
+				<ProfileBar
+					picture={this.props.user.photoURL}
+					userName={this.props.user.email.split('@')[0]}
+					onOpenText={this.handleOpenText}
+				/>
+				{this.renderOpenText()}
+				<MessageList messages={this.state.messages} />
+			</div>
 		)
 	}
 }
